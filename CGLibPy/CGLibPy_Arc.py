dfcce75f736d460f917ebab1d7fba3faf21f890c
ROHITVB\Rohit <rhtbapat@gmail.com>
2017-11-26 19:52:42 +0530

    Further Arc

--- CGLibPy/CGLibPy_Arc.py
+++ CGLibPy/CGLibPy_Arc.py
@@ -1,5 +1,7 @@
 class CGLibPy_Arc(object):
     centrePt = None
+    startPt = None
+    endPt = None
     radius = 0.0
     startAng = 0.0
     endAng = 0.0
@@ -7,4 +9,15 @@ class CGLibPy_Arc(object):
     fullCircle = False
     CCW = True
 
-    
+    def __init__(self,_args):
+        if _args.length() == 3:
+            self.centrePt = _args[0]
+            self.radius = _args[1]
+            self.CCW = _args[2]
+        elif _args.length() == 5:
+            self.centrePt = _args[0]
+            self.startPt = _args[1]
+            self.endPt = _args[2]
+            self.radius = _args[3]
+            self.CCW = _args[4]
+            
