18d67602aeb38ba916783477c627bf7e84d77b7f
ROHITVB\Rohit <rhtbapat@gmail.com>
2017-12-02 23:10:21 +0530

    Further improvements - Cross Products, dot products, lines parallel, perpendicular

--- CGLibPy/CGLibPy_Utility.py
+++ CGLibPy/CGLibPy_Utility.py
@@ -13,28 +13,72 @@ def dist2Pts(startPt,endPt):
     return distCoords(startPt.X,startPt.Y,startPt.Z,endPt.X,endPt.Y,endPt.Z)
 
 def dotProduct3D(vector1,vector2):
-    dp = vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2] 
+    dp = vector1.I*vector2.I + vector1.J*vector2.J + vector1.K*vector2.K 
     return dp
 
+def vectorLength(vec):
+    return math.sqrt(vec.I*vec.I + vec.J*vec.J + vec.K*vec.K)
+
 def crossProduct3D(vector1,vector2):
-    a = vector1[1]*vector2[2]-vector1[2]*vector2[1]
-    b = vector1[2]*vector2[0]-vector1[0]*vector2[2]
-    c = vector1[0]*vector2[1]-vector1[1]*vector2[0]
+    a = vector1.J*vector2.K-vector1.K*vector2.J
+    b = vector1.K*vector2.I-vector1.I*vector2.K
+    c = vector1.I*vector2.J-vector1.J*vector2.I
     cp = [a,b,c]
     return cp
 
+def ang2VectorsRadian(vec1,vec2):
+    numer = dotProduct3D(vec1,vec2)
+    denom = vectorLength(vec1)*vectorLength(vec2)
+    ang = math.acos(numer/denom)
+    '''
+    while ang <= -M_PI:
+        ang = ang + M_2PI
+    return ang
+    '''
+    return ang
+
+def ang2VectorsDegree(vec1,vec2):
+    return math.degrees(ang2VectorsRadian(vec1,vec2))
+
+def ang2LinesRadian(line1,line2):
+    return ang2VectorsRadian(line1.vec,line2.vec)
+
+def ang2LinesDegrees(line1,line2):
+    return ang2VectorsDegree(line1.vec,line2.vec)
+
+def areVectorsPerpendicular(vec1,vec2):
+    dP = dotProduct3D(vec1,vec2) 
+    if(math.isclose(dP,0.0)):
+        return True
+    else:
+        return False
+
+def areLinesPerpendicular(line1,line2):
+    return areVectorsPerpendicular(line1.vec,line2.vec) 
+
+def areVectorsParallel(vec1,vec2):
+    cP = crossProduct3D(vec1,vec2)
+    vecLen = math.sqrt(cP[0]*cP[0] + cP[1]*cP[1] + cP[2]*cP[2])
+    if(math.isclose(vecLen,0.0)):
+        return True
+    else:
+        return False
+
+def areLinesParallel(line1,line2):
+    return areVectorsParallel(line1.vec,line2.vec)
+
 def getDistance2DXY(point1,point2):
-    d = sqrt(sqr(point2[0]-point1[0]) + sqr(point2[1]-point1[1]))
+    d = sqrt(sqr(point2.X-point1.X) + sqr(point2.Y-point1.Y))
     return d
 
 
 def getDistance2DYZ(point1,point2):
-    d = sqrt(sqr(point2[1]-point1[1]) + sqr(point2[2]-point1[2]))
+    d = sqrt(sqr(point2.Y-point1.Y) + sqr(point2.Z-point1.Z))
     return d
 
 
 def getDistance2DXZ(point1,point2):
-    d = sqrt(sqr(point2[0]-point1[0]) + sqr(point2[2]-point1[2]))
+    d = sqrt(sqr(point2.X-point1.X) + sqr(point2.Z-point1.Z))
     return d
 
 def lineLineIntersection3DPointsParameters(p1,p2,p3,p4):
--- tests/createBasicGeos.py
+++ tests/createBasicGeos.py
@@ -6,8 +6,9 @@ import CGLibPy
 
 point = CGLibPy.CGLibPy_Point(10,20,30)
 
-line = CGLibPy.CGLibPy_Line([0,0,0,10,10,10])
+line = CGLibPy.CGLibPy_Line([0,0,0,10,0,0])
 line2 = CGLibPy.CGLibPy_Line([10,10,0,10,10,20])
+line3 = CGLibPy.CGLibPy_Line([0,0,0,0,10,0])
 
 print(point.X)
 print(line.startPt.Z)
@@ -15,4 +16,7 @@ print(line.midpt.Y)
 print(line.vec.K)
 print(line.lineLen)
 print(CGLibPy.CGLibPy_Utility.lineLineIntersection3D(line,line2))
+print(CGLibPy.CGLibPy_Utility.ang2LinesDegrees(line,line2))
+print(CGLibPy.CGLibPy_Utility.areLinesPerpendicular(line,line3))
+print(CGLibPy.CGLibPy_Utility.areLinesParallel(line,line3))
 
